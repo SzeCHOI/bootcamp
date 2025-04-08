994a42b467b68b3088aa4bc8c4196bbbf558c322
sze <novacafeltd@hotmail.com>
2025-04-08 11:49:30 +0800

    update5

---
+++ Helloworld.java
@@ -0,0 +1,19 @@
+public class Helloworld {
+  public static void main(String[] args) {
+    // hahahahah 
+
+    // x is a varibale, which is a "space" to store integer value ONLY.
+    // 3 is integer vallue.
+    // x is an integer variable.
+    // You are putting 3 into the variable.
+    // from right to left.
+
+    int x = -15;
+
+    // Print out the value of x
+    System.out.println(x);
+
+    System.out.println("Welcome to bootcamp!!!");
+
+  }
+}
